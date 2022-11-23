80ad9b294fbf16313016d63cf313cd8ac6c08cc9
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2022-11-23 08:27:34 -0500

    Update ejercicio3.sql

--- Session2/ejercicio3.sql
+++ Session2/ejercicio3.sql
@@ -3,5 +3,7 @@ select
     count(distinct fix.act_acct_cd) as num_users
 from "db-analytics-prod"."fixed_cwp" as fix
     inner join "db-stage-dev"."so_hdr_cwc" as service on cast(fix.act_acct_cd as bigint) = service.account_id
-    inner join "db-stage-prod"."interactions_cwp"  as interaction on interaction.account_id = fix.act_acct_cd
+    inner join "db-stage-prod"."interactions_cwp"  as interaction on cast(interaction.account_id as bigint) = service.account_id
+where date(fix.dt) = date_trunc('month', date(fix.dt))
+    and date(fix.dt) >= date('2022-01-01')
 group by 1 order by 1 
