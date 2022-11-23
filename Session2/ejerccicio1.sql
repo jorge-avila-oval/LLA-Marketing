89a4071119991ffbeb7382507dd3cdd58a322ab5
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2022-11-23 08:03:49 -0500

    Update ejerccicio1.sql

--- Session2/ejerccicio1.sql
+++ Session2/ejerccicio1.sql
@@ -5,6 +5,6 @@ select
     
 from "db-analytics-prod"."fixed_cwp" as fix
     left join "db-stage-prod"."interactions_cwp"  as interaction on interaction.account_id = fix.act_acct_cd
-where interaction.interaction_purpose_descrip = 'TICKET'
+where interaction.interaction_purpose_descrip = 'TRUCKROLL'
     and pd_bb_prod_cd is not null 
 group by 1,2 order by  1
