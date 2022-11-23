42ee887175822fd8e25ab0676e3638598ad12143
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2022-11-23 08:10:48 -0500

    Update ejerccicio1.sql

--- Session2/ejerccicio1.sql
+++ Session2/ejerccicio1.sql
@@ -7,4 +7,6 @@ from "db-analytics-prod"."fixed_cwp" as fix
     left join "db-stage-prod"."interactions_cwp"  as interaction on interaction.account_id = fix.act_acct_cd
 where interaction.interaction_purpose_descrip = 'TRUCKROLL'
     and pd_bb_prod_cd is not null 
+    and date(fix.dt) = date_trunc('month', date(fix.dt))
+    and date(fix.dt) >= date('2022-01-01')
 group by 1,2 order by  1
