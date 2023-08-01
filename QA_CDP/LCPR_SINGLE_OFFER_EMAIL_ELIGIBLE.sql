8409c8f3112cd1ac767c54193f3d545b88a4d697
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-08-01 14:04:37 -0500

    Update LCPR_SINGLE_OFFER_EMAIL_ELIGIBLE.sql

--- QA_CDP/LCPR_SINGLE_OFFER_EMAIL_ELIGIBLE.sql
+++ QA_CDP/LCPR_SINGLE_OFFER_EMAIL_ELIGIBLE.sql
@@ -1,66 +1,45 @@
-with 
-
-csr_customers AS (
+-- LCPR_SINGLE_OFFER_ELIGIBLE
+WITH
+csr_attributes as(
     SELECT 
-        account_id,
-        sub_acct_no_sbb as cust_acct,
-        as_of as csr_dte,
-        CASE WHEN welcome_offer = 'X' THEN true ELSE false END as welcome_offer,
-        CASE WHEN acp = 'X' THEN true ELSE false END as subsidize_flag,
-        CASE WHEN joint_customer = 'X' THEN true ELSE false END as joint_customer,
-        CASE 
-            WHEN  
-                substring(bill_code ,1,1) IN ('R','F') AND 
-                substring(bill_code ,2,1) IN ('1','2','3','4','5','6','7') 
-                THEN true ELSE false END    as valid_pckg,
-        CASE 
-            WHEN substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)  )-1,2) in ('24', '99', '67', '41', '19') then 'Control A'
-            WHEN substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)  )-1,2) in ('25', '47', '79', '22', '93') then 'Control X' 
-            WHEN  substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)   )-1,2) in ('26', '48', '80', '23', '94') then 'Control J' ELSE 'Target'
-        END as regime,
-        addr1_hse as CUST_ADDR1,
-        home_phone_sbb as PHONE_1,
-        bus_phone_sbb as PHONE_2,
-        cyc_cde_sbb as INVOICE_DAY,
-        email as EMAIL,
-        cust_typ_sbb AS CUST_TYPE,
-        delinquency_days AS DELINQUENCY_DAYS,
-        hsd AS HSD,
-        bill_code AS PCK_CODE,
-        res_name_sbb AS CUST_NAME,
-        *
-    FROM "prod"."public"."insights_customer_services_rates_lcpr"
+        account_id as numero_cuenta
+    from  "prod"."public"."insights_customer_services_rates_lcpr"
     WHERE AS_OF = (select max(as_of) from "prod"."public"."insights_customer_services_rates_lcpr" )
 ),
 
-offers as (
-    SELECT 
-        *
-    FROM "prod"."public"."lcpr_offers"
-
-),
-
-flagging as (
-    SELECT 
-            
-    account_id,
+flagging_attributes as (
+select 
+    *,
+    sub_acct_no_sbb as cust_acct,
     case when has_privacy_flag = 'X' then true else false end as privacy_flag ,
     case when has_open_order  = 'X' then true else false end as open_order ,
     case when pending_tc  = 'X' then true else false end as trouble_call ,
-    case when is_in_ndnc  = 'X' then true else false end as dnt_call_flag 
-    FROM "prod"."public"."flagging"
+    case when is_in_ndnc  = 'X' then true else false end as dnt_call_flag
+from "prod"."public"."flagging"
+-- limit 100
+),
+offers as (
+    SELECT 
+        account_id AS num_id,
+        *
+    FROM "prod"."public"."lcpr_offers"
 
 )
 
-SELECT 
-    count(distinct csr_customers.account_id)
-FROM csr_customers LEFT JOIN offers on  csr_customers.account_id = offers.account_id LEFT JOIN flagging ON csr_customers.account_id = flagging.account_id
-where 
--- condiciones CSR
-        EMAIL is not null and
-        HSD = 1  and 
-        -- condiciones offers
-        channel = 'email' and
-        -- next_best_action_date = current day por validar como hacer esa condicion
-        -- condiciones flagging 
-        privacy_flag = false
+select 
+    count(distinct CSR_FLAGG.numero_cuenta)
+FROM 
+(SELECT 
+    *
+FROM csr_attributes LEFT JOIN flagging_attributes 
+ON csr_attributes.numero_cuenta = flagging_attributes.account_id) CSR_FLAGG LEFT JOIN offers ON CSR_FLAGG.numero_cuenta = offers.account_id
+WHERE 
+    --condiciones offers
+    -- offers.next_best_action_date = and 
+    lower(offers.offer_type) = 'single' and 
+    offers.rank = 1 and 
+    -- offers.next_best_action_date = select current_date
+
+    
+    --condiciones flagging
+    CSR_FLAGG.privacy_flag = false -- and
