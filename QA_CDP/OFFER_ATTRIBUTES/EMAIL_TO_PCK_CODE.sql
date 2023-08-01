88d338745a0084a3c7858fcf9cc8033be9d778d3
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-08-01 16:48:32 -0500

    Create EMAIL_TO_PCK_CODE.sql

---
+++ QA_CDP/OFFER_ATTRIBUTES/EMAIL_TO_PCK_CODE.sql
@@ -0,0 +1,21 @@
+with csr_id as (
+    SELECT 
+        account_id as numero_cuenta
+    FROM  "prod"."public"."insights_customer_services_rates_lcpr"
+    WHERE AS_OF = (select max(as_of) from "prod"."public"."insights_customer_services_rates_lcpr" )
+
+),
+
+offers as (
+    SELECT
+        account_id,
+        MAX (
+    CASE WHEN lower(channel ) = 'email' then recommendedpkg ELSE NULL END) AS EMAIL_TO_PCK_CODE
+    from "prod"."public"."lcpr_offers"
+    group by 1
+)
+SELECT 
+    count(distinct account_id)
+FROM csr_id LEFT JOIN offers offers ON csr_id.numero_cuenta = offers.account_id
+WHERE EMAIL_TO_PCK_CODE is not null  -- poner la condición que se quiere probar en cada uno de los atributos. 
+;
