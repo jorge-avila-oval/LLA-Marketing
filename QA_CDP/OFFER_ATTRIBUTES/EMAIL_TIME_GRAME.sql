43fcbb4fdc6ca44113036e592b2f897fd1ac5216
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-08-01 18:14:10 -0500

    Create EMAIL_TIME_GRAME.sql

---
+++ QA_CDP/OFFER_ATTRIBUTES/EMAIL_TIME_GRAME.sql
@@ -0,0 +1,20 @@
+with csr_id as (
+    SELECT 
+        account_id as numero_cuenta
+    FROM  "prod"."public"."insights_customer_services_rates_lcpr"
+    WHERE AS_OF = (select max(as_of) from "prod"."public"."insights_customer_services_rates_lcpr" )
+
+),
+
+offers as (
+    SELECT
+        account_id,
+        MAX (CASE WHEN lower( channel )= 'email' then time_frame  ELSE NULL END) AS EMAIL_TIME_FRAME
+    from "prod"."public"."lcpr_offers"
+    group by 1
+)
+SELECT 
+    count(distinct numero_cuenta)
+FROM csr_id LEFT JOIN offers offers ON csr_id.numero_cuenta = offers.account_id
+WHERE lower(EMAIL_TIME_FRAME) like('afternoon')  -- poner la condición que se quiere probar en cada uno de los atributos. 
+;
