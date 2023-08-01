5b100a150c92e02770c4e7872a07d1aab9a885b3
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-08-01 17:57:10 -0500

    Create OFFER_TYPE.sql

---
+++ QA_CDP/OFFER_ATTRIBUTES/OFFER_TYPE.sql
@@ -0,0 +1,19 @@
+with csr_id as (
+    SELECT 
+        account_id as numero_cuenta
+    FROM  "prod"."public"."insights_customer_services_rates_lcpr"
+    WHERE AS_OF = (select max(as_of) from "prod"."public"."insights_customer_services_rates_lcpr" )
+
+),
+
+offers as (
+    SELECT
+        account_id,
+        offer_type
+    from "prod"."public"."lcpr_offers"
+    -- group by 1
+)
+SELECT 
+    count(distinct numero_cuenta)
+FROM csr_id LEFT JOIN offers offers ON csr_id.numero_cuenta = offers.account_id
+WHERE lower(offer_type) like ('%single%') -- poner la condición que se quiere probar en cada uno de los atributos. 
