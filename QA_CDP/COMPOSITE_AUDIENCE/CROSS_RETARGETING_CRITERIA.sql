c711dd557b20c8973719f9db9f634edbae39d705
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-08-23 13:37:35 -0500

    Create CROSS_RETARGETING_CRITERIA.sql

---
+++ QA_CDP/COMPOSITE_AUDIENCE/CROSS_RETARGETING_CRITERIA.sql
@@ -0,0 +1,49 @@
+with
+csr_attributes as (
+    SELECT 
+        account_id as numero_cuenta,
+        sub_acct_no_sbb as cust_acct,
+        as_of as csr_dte,
+        CASE WHEN welcome_offer = 'X' THEN true ELSE false END as welcome_off,
+        CASE WHEN acp = 'X' THEN true ELSE false END as subsidize_fl,
+        CASE WHEN joint_customer = 'X' THEN true ELSE false END as joint_cust,
+        CASE 
+            WHEN  
+                substring(bill_code ,1,1) IN ('R','F') AND 
+                substring(bill_code ,2,1) IN ('1','2','3','4','5','6','7') 
+                THEN true ELSE false END    as valid_pckg,
+        CASE 
+            WHEN substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)  )-1,2) in ('24', '99', '67', '41', '19') then 'Control A'
+            WHEN substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)  )-1,2) in ('25', '47', '79', '22', '93') then 'Control X' 
+            WHEN  substring(cast(sub_acct_no_sbb as varchar)  , length(cast(sub_acct_no_sbb as varchar)   )-1,2) in ('26', '48', '80', '23', '94') then 'Control J' ELSE 'Target'
+        END as regime,
+        addr1_hse as CUST_ADDR1,
+        home_phone_sbb as PHONE_1,
+        bus_phone_sbb as PHONE_2,
+        cyc_cde_sbb as INVOICE_DAY,
+        email as EMAIL,
+        cust_typ_sbb AS CUST_TYPE,
+        delinquency_days AS DELINQUENCY_DAYS,
+        hsd AS HSD,
+        bill_code AS pck_code_csr,
+        res_name_sbb AS CUST_NAME,
+        tenure
+            from "prod"."public"."lcpr_dna_fixed"
+   WHERE AS_OF = (select max(as_of) from "prod"."public"."lcpr_dna_fixed")
+    -- limit 100
+),
+
+migrations as (
+    select 
+        CAST(DATEADD(SECOND, ls_chg_dte_ocr_ms/1000,'1970/1/1') AS DATE) AS chg_date_ocr,
+        *
+    from  "prod"."public"."lcpr_last_transaction_orderactivity"
+
+)
+
+select 
+    count (distinct numero_cuenta)
+from csr_attributes left join migrations on numero_cuenta = migrations.account_id
+where 
+    chg_date_ocr  >=  DATEADD(month, -3, CURRENT_DATE) or
+    tenure <= 0.5
