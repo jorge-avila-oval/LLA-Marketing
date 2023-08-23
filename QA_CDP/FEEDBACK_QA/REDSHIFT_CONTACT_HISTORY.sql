ffd628fb4a3f1ced8297cf4b2cc0cd7e61c4c9d6
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-08-23 17:01:25 -0500

    Update REDSHIFT_CONTACT_HISTORY.sql

--- QA_CDP/FEEDBACK_QA/REDSHIFT_CONTACT_HISTORY.sql
+++ QA_CDP/FEEDBACK_QA/REDSHIFT_CONTACT_HISTORY.sql
@@ -1,5 +1,22 @@
 SELECT 
-    *
+    account_id,
+    channel, 
+    campaign,
+    use_case,
+    CAST(DATEADD(SECOND, sent_dt_ms/1000,'1970/1/1') AS DATE) as sent_dt,
+    sent_dt_ms,
+    CAST(DATEADD(SECOND, contact_dt_ms/1000,'1970/1/1') AS DATE) as contact_dt,
+    contact_dt_ms,
+    CAST(DATEADD(SECOND, conv_dt_ms/1000,'1970/1/1') AS DATE) as conv_dt,
+    conv_dt_ms,
+    conv_type,
+    CAST(DATEADD(SECOND, rec_dt_ms/1000,'1970/1/1') AS DATE) as rec_dt, 
+    rec_dt_ms,
+    order_id,
+    conv_use_case_category,
+    dt,
+    dt_ms
 FROM "prod"."public"."lcpr_communications_hst"
 WHERE channel = 'email' and sent_dt_ms is not null and 
-    campaign  in ('TEST_20230823_CBM_CDP_OFFERFIT_CONTROL')
+    campaign  in ('TEST_20230823_CBM_CDP_OFFERFIT_CONTROL', 'TEST_20230823_CBM_CDP_OFFERFIT_CONTROL')
+    
