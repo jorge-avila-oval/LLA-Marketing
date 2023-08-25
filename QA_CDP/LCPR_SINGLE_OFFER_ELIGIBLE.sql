022943f44b03ba7105c4808227c099cdb3c7b6ef
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-08-24 23:47:15 -0500

    Update LCPR_SINGLE_OFFER_ELIGIBLE.sql

--- QA_CDP/LCPR_SINGLE_OFFER_ELIGIBLE.sql
+++ QA_CDP/LCPR_SINGLE_OFFER_ELIGIBLE.sql
@@ -1,35 +1,36 @@
 WITH
 csr_attributes as(
     SELECT 
-        account_id as numero_cuenta,
-        *
+        account_id as numero_cuenta
     from  "prod"."public"."insights_customer_services_rates_lcpr"
     WHERE AS_OF = (select max(as_of) from "prod"."public"."insights_customer_services_rates_lcpr" )
+),
+flagging_attributes as (
+select
+    account_id,
+    case when has_privacy_flag = 'X' then true else false end as privacy_flag 
+from "prod"."public"."flagging"
 ),
 offers as (
     SELECT 
         account_id AS num_id,
-        *
+        offer_type,
+        rank,
+        next_best_action_date
     FROM "prod"."public"."lcpr_offers"
--- limit 100
-),
-cs_features as (
-    SELECT 
-        account_id AS numero,
-        lst_bill_dt, 
-        CAST(DATEADD(SECOND, lst_bill_dt/1000,'1970/1/1') AS DATE) AS converted_date, 
-        current_date-10 AS period_evaluated
-    FROM "prod"."public"."lcpr_customer_service_features"
 )
 select 
-    count(distinct numero_cuenta)
+    count(distinct CSR_FLAGG.numero_cuenta)
 FROM 
 (SELECT 
     *
-FROM csr_attributes LEFT JOIN offers 
-ON csr_attributes.numero_cuenta = offers.num_id) CSR_OFFERS LEFT JOIN cs_features ON csr_offers.numero_cuenta = cs_features.numero
+FROM csr_attributes LEFT JOIN flagging_attributes 
+ON csr_attributes.numero_cuenta = flagging_attributes.account_id) CSR_FLAGG LEFT JOIN offers ON CSR_FLAGG.numero_cuenta = offers.num_id
 WHERE 
-    hsd=1 and
-    channel = 'email' and 
-    email is not null  and 
-    converted_date <= period_evaluated
+    --condiciones offers
+    lower(offers.offer_type) = 'single' and 
+    offers.rank = 1 and 
+    next_best_action_date= current_date AND
+    
+    --condiciones flagging
+    CSR_FLAGG.privacy_flag = false 
