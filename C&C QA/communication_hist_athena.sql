0fc7a742bb0c689cecaa1b33c80ce360aaf35dcb
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-08-31 07:45:13 -0500

    Update communication_hist_athena.sql

--- C&C QA/communication_hist_athena.sql
+++ C&C QA/communication_hist_athena.sql
@@ -1,3 +1,6 @@
 SELECT 
-  * 
-FROM "db_dev_cdp_project"."communications_hist" where day='20230824'
+    *
+FROM "db_dev_cdp_project"."communications_hist" 
+where 
+    -- day='20230830'-- vista del día
+    comm_dt >= date('2023-08-29') -- vista acumulada
