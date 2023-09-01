e0bd5bd53d8c7d291bf2320c833e9ecf12b731f0
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-09-01 17:27:09 -0500

    Update Converted Customers RS.sql

--- C&C QA/Converted Customers RS.sql
+++ C&C QA/Converted Customers RS.sql
@@ -18,3 +18,4 @@ select
     *
 from contacted_cust inner join last_transaction on contacted_cust.account_id = last_transaction.account_id
 where ord_date >= sent_date
+    AND contact_dt_ms is not null 
