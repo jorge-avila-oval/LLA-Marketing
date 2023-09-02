2c936f4c2912f42a56c5e360f0d26f96dac10ef5
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-09-02 00:01:25 -0500

    Create Validación Comm hist Redshift.sql

---
+++ C&C QA/Validación Comm hist Redshift.sql
@@ -0,0 +1,34 @@
+with comms_hist as (
+SELECT 
+    account_id,
+    channel, 
+    campaign,
+    use_case,
+    CAST(DATEADD(SECOND, sent_dt_ms/1000,'1970/1/1') AS DATE) as sent_dt,
+    sent_dt_ms,
+    CAST(DATEADD(SECOND, contact_dt_ms/1000,'1970/1/1') AS DATE) as contact_dt,
+    contact_dt_ms,
+    CAST(DATEADD(SECOND, conv_dt_ms/1000,'1970/1/1') AS DATE) as conv_dt,
+    conv_dt_ms,
+    conv_type,
+    CAST(DATEADD(SECOND, rec_dt_ms/1000,'1970/1/1') AS DATE) as rec_dt, 
+    rec_dt_ms,
+    order_id,
+    conv_use_case_category,
+    dt,
+    dt_ms
+FROM "prod"."public"."lcpr_communications_hst"
+WHERE 
+    channel = 'email' and 
+    sent_dt >= date('2023-08-29') and 
+  -- condición para incluir el nombre de los automation program que enviaron los correos
+    campaign  in ('CBM_CDP_OFFERFIT_UPSELLING_TARGET', 'CBM_CDP_OFFERFIT_UPSELLING_CONTROL')
+)
+
+select 
+    distinct sent_dt, 
+    contact_dt,
+    count(distinct account_id)
+from comms_hist
+group by 1,2
+order by 1,2
