e7a3607d1d61983a8f040e0e5cf7e71e186f8a45
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-09-05 08:36:43 -0500

    Update communication_hist_athena.sql

--- C&C QA/communication_hist_athena.sql
+++ C&C QA/communication_hist_athena.sql
@@ -4,3 +4,5 @@ FROM "db_dev_cdp_project"."communications_hist"
 where 
     -- day='20230830'-- vista del día
     comm_dt >= date('2023-08-29') -- vista acumulada
+    and campaign_name in 
+        ('CBM_CDP_OFFERFIT_UPSELLING_TARGET','CBM_CDP_OFFERFIT_UPSELLING_CONTROL')
