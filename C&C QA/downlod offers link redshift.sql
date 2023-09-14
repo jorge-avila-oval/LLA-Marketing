1efe96f6d413d23bc7280ab22ea8324563b824b7
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-09-13 22:14:48 -0500

    Create downlod offers link redshift.sql

---
+++ C&C QA/downlod offers link redshift.sql
@@ -0,0 +1,11 @@
+UNLOAD (
+'select 
+*   
+from "prod"."public"."lcpr_offers"
+where CAST(DATEADD(SECOND, link_dt/1000,''1970/1/1'') AS DATE) = date(''2023-09-11'') and link is not null ' )
+TO 's3://aiq-exchange-lcpr/exploration/Redshift Exports/20230911_offers_links'
+IAM_ROLE   'arn:aws:iam::283229902738:role/redshift_s3_access'
+PARALLEL OFF
+HEADER
+ALLOWOVERWRITE
+CSV
