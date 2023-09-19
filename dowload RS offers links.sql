dc8539c205f27538604a19facfa862bde1e2ecad
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-09-19 07:38:57 -0500

    Update dowload RS offers links.sql

--- dowload RS offers links.sql
+++ dowload RS offers links.sql
@@ -1,12 +1,13 @@
 UNLOAD (
-'select 
+'
+select 
 *   
 from "prod"."public"."lcpr_offers"
-where link_dt = 1694044800000 and link is not null ' )
-TO 's3://aiq-exchange-lcpr/exploration/Redshift Exports/20230907_offers_links'
+where CAST(DATEADD(SECOND, link_dt/1000,''1970/1/1'') AS DATE)  = date(''2023-09-19'') and link is not null 
+' )
+TO 's3://aiq-exchange-lcpr/exploration/Redshift Exports/20230919_offers_links'
 IAM_ROLE   'arn:aws:iam::283229902738:role/redshift_s3_access'
 PARALLEL OFF
 HEADER
 ALLOWOVERWRITE
 CSV
-
