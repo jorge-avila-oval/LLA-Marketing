5b8a0e3471abdd9a03e763ccfc7cd93b271b0060
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-09-28 07:39:35 -0500

    Update dowload RS offers links.sql

--- dowload RS offers links.sql
+++ dowload RS offers links.sql
@@ -3,9 +3,9 @@ UNLOAD (
 select 
 *   
 from "prod"."public"."lcpr_offers"
-where CAST(DATEADD(SECOND, link_dt/1000,''1970/1/1'') AS DATE)  = date(''2023-09-19'') and link is not null 
+where CAST(DATEADD(SECOND, link_dt/1000,''1970/1/1'') AS DATE)  = date(''2023-09-28'') and link is not null 
 ' )
-TO 's3://aiq-exchange-lcpr/exploration/Redshift Exports/20230919_offers_links'
+TO 's3://aiq-exchange-lcpr/exploration/Redshift Exports/offers_links_20230928'
 IAM_ROLE   'arn:aws:iam::283229902738:role/redshift_s3_access'
 PARALLEL OFF
 HEADER
