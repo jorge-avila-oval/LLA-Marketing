bfcf734d1dc2224bc9816df3421a40f26c4d30e7
jorge-avila-oval <118453205+jorge-avila-oval@users.noreply.github.com>
2023-10-13 16:35:31 -0500

    Update Converted delta arpu athena.sql

--- C&C QA/Converted delta arpu athena.sql
+++ C&C QA/Converted delta arpu athena.sql
@@ -3,21 +3,14 @@ communication_hist as
 (
 SELECT 
     *
-FROM "db_dev_cdp_project"."conv_communications_hist" 
+FROM 
+    "db_dev_cdp_project"."communications_hist" 
 where 
     -- day='20230830'-- vista del día
     comm_dt >= date('2023-08-29')
     and conv_flg = true
-),
-
-contact_data as (
-    select 
-        lcpr_aiq_id,
-        lcpr_lla_id, 
-        externalid,
-        offer_from_pck_code
-    FROM "db_dev_cdp_project"."emarsys_contact_data"
-    limit 100
+    -- and channel = 'Outbound_call'
+    and channel = 'email'
 ),
 
 aiq_converted_candidates as (
@@ -33,12 +26,20 @@ aiq_converted_candidates as (
 transaction_order as 
 (
     SELECT 
-        -- month_amt_ocr - bef_value_amt as delta_arpu_TO,
         order_no_ooi,
-        SUBSTRING(PHONE_UP,1,5) as phone_bundle,
-        SUBSTRING(CABLE_UP,1,5) as cable_bundle,
-        SUBSTRING(INTERNET_UP,1,5) as internet_bundle
+        CASE 
+            WHEN ord_typ <> 'DOWNGRADE' THEN SUBSTRING(PHONE_UP,1,5) 
+            ELSE SUBSTRING(phone_down,1,5) END as phone_bundle,
+        CASE    
+            WHEN ord_typ <> 'DOWNGRADE' THEN SUBSTRING(CABLE_UP,1,5)
+            ELSE SUBSTRING(CABLE_DOWN,1,5) END AS cable_bundle,
+        CASE 
+            WHEN ord_typ <> 'DOWNGRADE' THEN SUBSTRING(INTERNET_UP,1,5) 
+            ELSE SUBSTRING(INTERNET_DOWN,1,5) END as internet_bundle,
+        (CASE WHEN ("lower"(stb_sc) LIKE '%dvr%') THEN 1 ELSE 0 END) new_dvr_flag
     FROM "db-stage-dev"."transactions_orderactivity"
+    -- WHERE order_no_ooi IN (1001142244081102, 1001142239001107)
+    -- limit 100
 ),
 
 
@@ -54,7 +55,7 @@ assets_pck_price as
 (
 select 
     assets."pkg_cde_pks" as pkg_cde_pks,
-    sum(assets.chrg_pks) as total_pkg_price
+    round(sum(assets.chrg_pks), 2) as total_pkg_price
 from "db-stage-prod"."insights_assets_lcpr" assets 
     inner join assets_max_dt on assets.pkg_cde_pks = assets_max_dt.pkg_cde_pks and 
     dt = max_dt
@@ -66,7 +67,9 @@ from_pck_code as (
         lcpr_lla_id,
         externalid,
         lcpr_aiq_id,
-        offer_from_pck_code
+        offer_from_pck_code,
+        cmp1_upselling_offerfit_discount as discount,
+        cmp1_upselling_offerfit_to_csg_code as recomm_pkg
     from "db_dev_cdp_project"."emarsys_contact_data"
     where lcpr_aiq_id is not null
     -- limit 100
@@ -78,30 +81,30 @@ from_to_pck_migrations as
         distinct 
         account_id,
         sent_dt,
+        contact_dt,
         conversion_dt,
         lcpr_aiq_id,
         order_id,
         offer_from_pck_code,
         case when cable_bundle is not null and internet_bundle is not null and phone_bundle is not null
         and cable_bundle = internet_bundle and cable_bundle = phone_bundle then cable_bundle
-        
         when cable_bundle is not null and internet_bundle is not null and phone_bundle =' '
         and cable_bundle = internet_bundle then cable_bundle
-        
         when cable_bundle is not null and internet_bundle =' ' and phone_bundle is not null
         and cable_bundle = phone_bundle then cable_bundle
-        
         when cable_bundle =' ' and internet_bundle is not null and phone_bundle is not null
         and internet_bundle = phone_bundle then internet_bundle
-        
         when cable_bundle is not null and internet_bundle =' ' and phone_bundle =' ' then cable_bundle
         when internet_bundle is not null and cable_bundle =' ' and phone_bundle =' ' then internet_bundle
         when phone_bundle is not null and internet_bundle =' ' and cable_bundle =' ' then phone_bundle
-        else null end as new_bundle
+        else null end as new_bundle,
+        new_dvr_flag,
+        cast(discount as double) as discount,
+        recomm_pkg
         
     from  communication_hist
-        inner join from_pck_code on communication_hist.account_id = from_pck_code.lcpr_aiq_id 
-        inner join transaction_order on order_id = cast(transaction_order.order_no_ooi as varchar)
+        LEFT join from_pck_code on communication_hist.account_id = from_pck_code.lcpr_aiq_id 
+        LEFT join transaction_order on order_id = cast(transaction_order.order_no_ooi as varchar)
     -- where conv_flg = true
 ),
 
@@ -109,16 +112,27 @@ final_delta_arpu as
 (
 select 
     from_to_pck_migrations.*,
-    from_pck_price.total_pkg_price,
-    to_pck_price.total_pkg_price,
-    to_pck_price.total_pkg_price - from_pck_price.total_pkg_price as delta_arpu
+    from_pck_price.total_pkg_price as from_price,
+    -- to_pck_price.total_pkg_price as to_price,
+    CASE WHEN (from_to_pck_migrations.new_dvr_flag = 1) THEN (to_pck_price.total_pkg_price + 5) ELSE (to_pck_price.total_pkg_price) END AS TO_PRICE_DVR,
+    -- to_pck_price.total_pkg_price - from_pck_price.total_pkg_price as delta_arpu,
+    CASE 
+        WHEN new_bundle = recomm_pkg then 
+            (
+                CASE 
+                    WHEN (from_to_pck_migrations.new_dvr_flag = 1) and discount = 15 THEN (to_pck_price.total_pkg_price + 5 - (15/12)) -from_pck_price.total_pkg_price
+                    WHEN (from_to_pck_migrations.new_dvr_flag = 0) and (discount = 15) then (to_pck_price.total_pkg_price - (15/12)) - from_pck_price.total_pkg_price 
+                    WHEN (from_to_pck_migrations.new_dvr_flag = 1) THEN (to_pck_price.total_pkg_price + 5) - from_pck_price.total_pkg_price 
+                    ELSE (to_pck_price.total_pkg_price) - from_pck_price.total_pkg_price   END
+            )
+        WHEN (from_to_pck_migrations.new_dvr_flag = 1)  THEN (to_pck_price.total_pkg_price + 5) - from_pck_price.total_pkg_price
+        WHEN (from_to_pck_migrations.new_dvr_flag = 0) THEN to_pck_price.total_pkg_price - from_pck_price.total_pkg_price END as delta_arpu_adjusted
+
 from from_to_pck_migrations 
-    inner join assets_pck_price from_pck_price on offer_from_pck_code = from_pck_price.pkg_cde_pks
-    inner join assets_pck_price to_pck_price on new_bundle = to_pck_price.pkg_cde_pks
+    LEFT join assets_pck_price from_pck_price on offer_from_pck_code = from_pck_price.pkg_cde_pks
+    LEFT join assets_pck_price to_pck_price on new_bundle = to_pck_price.pkg_cde_pks
 )
 
-select * from final_delta_arpu
-
--- select 
---     sum (delta_arpu)
--- from final_delta_arpu
+select 
+    *
+from final_delta_arpu
